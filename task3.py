a45fb73403eb7a7b674340db84ff8727a93c5070
sidd-duaa <123830170+sidd-duaa@users.noreply.github.com>
2023-03-19 20:27:20 +0500

    Initial

---
+++ task3.py
@@ -0,0 +1,50 @@
+# 2-1. Simple Message
+message = "Hello Python World!"
+print(message)
+
+# 2-2. Simple Messages
+greet = "Hi there!"
+print(greet)
+
+greet = "Hi everyone!"
+print(greet)
+
+# 2-3. Personal Message
+name = "madiha"
+print("Hi " + name.title() + "! Did you learn python?")
+
+# 2-4. Name Cases
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# 2-5. Famous Quote
+print('Albert Einstein once said, "A person who never made a mistake never tried something new"')
+
+# 2-6. Famous Quote 2
+famous_person = "Albert Einstein"
+famous_quote = famous_person + ' once said, "A person who never made a mistake never tried something new"'
+print(famous_quote)
+
+# 2-7. Stripping Names
+person = "\nMadiha\t"
+print(person.lstrip())
+print(person.rstrip())
+print(person.strip())
+
+# 2-8. Number Eight
+print(4+4)
+print(16-8)
+print(2*4)
+print(16/2)
+
+# 2-9. Favourite Number
+num = 3
+msg = "My favorite number is " + str(num) + " because it's my birthdate"
+print(msg)
+
+# 2-10. Adding Comments
+# The code is all about basics of Python
+
+# 2-11. Zen of Python
+import this
